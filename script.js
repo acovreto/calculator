f5df308336e4aac67f5e6a47c011f83130df4062
acovreto <55660827+acovreto@users.noreply.github.com>
2025-02-06 00:38:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
 class Calculator {
   operand1 = 0;
   operand2 = 0;
+  lastOparation = null;
   buttonsEl = document.querySelector(".calculator__keys");
   displayEl = document.querySelector(".calculator__output");
   constructor() {
     this.events();
   }
-  events() {
+   events() {
     this.buttonsEl.addEventListener("click", (e) => {
-      let number = e.target.textContent;
-      console.log(e.target.textContent == "+");
-      if (
-        number == "+" ||
-        number == "-" ||
-        number == "*" ||
-        number == "/" ||
-        number == "AC" ||
-        number == "="
-      ) {
-        console.log(e.target.textContent);
-      } else {
-        this.displayNumber(number);
+      let clickedButton = e.target.textContent;
+
+      if (clickedButton !== "=" && clickedButton !== "AC") {
+        this.displayInput(clickedButton);
+      } else if (clickedButton === "=") {
+        this.lastOparation = "=";
+        let expresion = this.displayEl.textContent;
+        this.displayEl.textContent = eval(expresion);
+      } else if (clickedButton === "AC") {
+        this.clearDisplay();
       }
     });
   }
-  displayNumber(number) {
+clearDisplay() {
+    this.displayEl.textContent = "0";
+  }
+  displayResult(result) {
+    this.displayEl.textContent = result;
+  }
+  displayInput(clickedButton) {
+    if (this.lastOparation == "=") {
+      this.lastOparation = clickedButton;
+      if (
+        clickedButton == "+" ||
+        clickedButton == "-" ||
+        clickedButton == "*" ||
+        clickedButton == "/"
+      ) {
+        this.displayEl.textContent += clickedButton;
+        return;
+      }
+      this.clearDisplay();
+    }
     if (this.displayEl.textContent == "0") {
-      this.displayEl.textContent = number;
+      this.displayEl.textContent = clickedButton;
     } else {
-      if (this.displayEl.textContent.includes(".") && number == ".") return;
-      this.displayEl.textContent += number;
+      this.displayEl.textContent += clickedButton;
     }
   }
 }
